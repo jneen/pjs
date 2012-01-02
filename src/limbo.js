08f39be6b79d553dccd023d7f01a44df7516bc42
Jay Adkisson <jjmadkisson@gmail.com>
2012-01-02 13:38:13 -0800

    minifier optimization - 460b

--- src/limbo.js
+++ src/limbo.js
@@ -4,8 +4,7 @@ var Limbo = (function(slice, prototype, hasOwnProperty, undefined) {
   function Limbo(_superclass, definition) {
     function C(args) {
       if (!(this instanceof C)) return new C(arguments);
-      if (!args) return;
-      if (isFunction(obj.init)) obj.init.apply(obj, args);
+      if (args && isFunction(obj.init)) obj.init.apply(obj, args);
     }
 
     if (definition === undefined) {
