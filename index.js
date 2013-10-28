c3a9e340199ce5a57e80aa608f8f9484b05b8451
http://jayferd.us/ <jjmadkisson@gmail.com>
2013-10-28 12:01:39 -0700

    just require package.json, no need for JSON.parse

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 exports.P = require('./build/p.commonjs').P;
-exports.version = JSON.parse(require('fs').readFileSync(__dirname + '/package.json')).version;
+exports.version = require('./package.json').version;
